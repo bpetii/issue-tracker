5f283983d4e916f18d338f82485cb8781b6cbaeb
Peter Biro <peter.biro@oliasoft.com>
2023-10-14 15:44:20 +0200

    Refactor: organize imports

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -2,15 +2,14 @@
 import dynamic from "next/dynamic";
 import "easymde/dist/easymde.min.css";
 import {useForm, Controller} from 'react-hook-form';
-import { Button, Callout, TextField, Text } from '@radix-ui/themes'
+import { Button, Callout, TextField } from '@radix-ui/themes'
 import axios from 'axios';
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import {zodResolver} from '@hookform/resolvers/zod';
 import { createIssueSchema } from "@/app/validationSchemas";
 import {z} from 'zod';
-import ErrorMessage from "@/app/components/ErrorMessage";
-import Spinner from "@/app/components/Spinner";
+import {ErrorMessage, Spinner} from "@/app/components";
 
 const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {ssr: false})
 
