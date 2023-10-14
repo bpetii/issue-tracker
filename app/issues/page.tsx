098e98bdfd005e85fe235b50268d3e80234fd50d
Peter Biro <peter.biro@oliasoft.com>
2023-10-14 15:26:14 +0200

    build a styled component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Table } from '@radix-ui/themes'
-import Link from 'next/link'
+import { Table} from '@radix-ui/themes'
+import Link from '../components/Link'
 import prisma from '@/prisma/client'
 import IssueStatusBadge from '../components/IssueStatusBadge'
 import delay from "delay";
-import { IssueActions } from './issueActions'
+import { IssueActions } from './IssueActions'
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany();
