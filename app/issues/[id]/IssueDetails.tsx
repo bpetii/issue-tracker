ce0a94863f1e1a95789c55eb53c96823ee0cdd17
Peter Biro <peter.biro@oliasoft.com>
2023-10-14 16:03:42 +0200

    Refactor apply Single Responsibility Principle

---
+++ app/issues/[id]/IssueDetails.tsx
@@ -0,0 +1,23 @@
+import { IssueStatusBadge } from '@/app/components'
+import { Issue } from '@prisma/client'
+import { Card, Flex, Heading } from '@radix-ui/themes'
+import ReactMarkdown from 'react-markdown'
+
+
+
+const IssueDetails = ({issue}: {issue: Issue}) => {
+  return (
+    <>
+        <Heading>{issue.title}</Heading>
+				<Flex gap='3' my='2'>
+					<IssueStatusBadge status={issue.status} />
+					<p>{issue.created_at.toDateString()}</p>
+				</Flex>
+				<Card className='prose' mt="4">
+						<ReactMarkdown>{issue.description}</ReactMarkdown>
+				</Card>
+    </>
+  )
+}
+
+export default IssueDetails
