37ad70c292c46ec71cf44710bfb53f36c8452de5
Peter Biro <peter.biro@oliasoft.com>
2023-10-15 22:58:48 +0200

    optimize perfomance by using react cache

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -7,18 +7,18 @@ import AssigneeSelect from './AssigneeSelect'
 import { DeleteIssueButton } from './DeleteIssueButton'
 import EditIssueButton from './EditIssueButton'
 import IssueDetails from './IssueDetails'
-import { Metadata } from 'next/types'
+import { cache } from 'react'
 
 interface Props {
     params: { id: string},
 }
 
+const fetchIssue = cache((issueId: number) => prisma.issue.findUnique({where: {id: issueId}}))
+
 const IssueDetailPage = async ({params}: Props) => {
 	const session = await getServerSession(authOptions);
 	if (typeof parseInt(params.id) !== 'number') notFound()
-	const issue = await prisma.issue.findUnique({
-		where: {id: parseInt(params.id)}
-	})
+	const issue = await fetchIssue(parseInt(params.id))
 
 	if (!issue) 
 		notFound();
@@ -42,9 +42,7 @@ const IssueDetailPage = async ({params}: Props) => {
 export const dynamic = 'force-dynamic';
 
 export async function generateMetadata({params}: Props){
-	const issue = await prisma.issue.findUnique({
-		where: {id: parseInt(params.id)}
-	})
+	const issue = await fetchIssue(parseInt(params.id))
 
 	return {
 		title: issue?.title,
