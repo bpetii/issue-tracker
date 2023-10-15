cf988cca14f2e4b4103c5c014e2db1ad5041937d
Peter Biro <peter.biro@oliasoft.com>
2023-10-15 09:17:49 +0200

    build assignee select component

---
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -0,0 +1,21 @@
+'use client';
+import { Select } from '@radix-ui/themes'
+import React from 'react'
+
+const AssigneeSelect = () => {
+  return (
+    <Select.Root>
+        <Select.Trigger placeholder='assign,,,'/>
+				<Select.Content>
+					<Select.Group>
+						<Select.Label>
+							Suggestions
+						</Select.Label>
+						<Select.Item value="1">Peter Biro</Select.Item>
+					</Select.Group>
+				</Select.Content>
+    </Select.Root>
+  )
+}
+
+export default AssigneeSelect
